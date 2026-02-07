bef95d9704397f476bc2f62b42b110e2adc80d07
jh7764 <jennahayek2007@gmail.com>
2026-02-06 20:43:39 -0500

    Add event listener for page load and initialize counter

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 window.addEventListener("load", () => {
+
+    const content = document.getElementById("content");
     const star = document.getElementById("star")
     const counter = document.getElementById("counter");
     const point = document.getElementsByClassName("point");
+    const nav = document.getElementById("nav-placeholder");
+
     
     let count = parseInt(localStorage.getItem("points")) || 0;
     counter.innerText = count;
